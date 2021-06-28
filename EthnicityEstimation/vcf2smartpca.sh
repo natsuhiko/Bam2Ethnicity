7461b3f3274b36dfc8743f7e8d1f4c4715745d5c
Natsuhiko Kumasaka <nk5@sanger.ac.uk>
2021-06-28 19:10:32 +0100

    1000GP data

--- EthnicityEstimation/vcf2smartpca.sh
+++ EthnicityEstimation/vcf2smartpca.sh
@@ -1,17 +1,24 @@
 
+
+PROGDIR=/nfs/team205/nk5/Applications/EthnicityEst
 VCFDIR=$1
 OUTDIR=$1/SmartPCA
 mkdir $OUTDIR
 
 TABIX=/software/team170/miniconda3/bin/tabix 
+SMARTPCA=/nfs/team205/nk5/Applications/EIG/bin/smartpca.perl
+
+# 1KG data
+THG=/nfs/team205/nk5/1000G/20181129
+
 
 # converting vcf to eigen format
 rm $OUTDIR/eigen.orig.gz
 for CHR in {1..22}
 do
 	$TABIX $VCFDIR/chr$CHR.vcf.gz $CHR | awk '{print $1"\t"$2-1"\t"$2}' | gzip > $OUTDIR/tmp.bed.gz
-	$TABIX -R $OUTDIR/tmp.bed.gz /nfs/team205/nk5/1000G/20181129/chr$CHR.vcf.gz | /nfs/team205/nk5/Applications/EthnicityEst/vcf2eigen CONV -N 2548 -dose -MAF 0.05 | gzip > $OUTDIR/tmp1.eigen.gz
-	$TABIX -R $OUTDIR/tmp.bed.gz $VCFDIR/chr$CHR.vcf.gz | /nfs/team205/nk5/Applications/EthnicityEst/vcf2eigen CONV -N 1 -dose -MAF -1.0 | gzip > $OUTDIR/tmp2.eigen.gz
+	$TABIX -R $OUTDIR/tmp.bed.gz $THG/chr$CHR.vcf.gz | $PROGDIR/vcf2eigen CONV -N 2548 -dose -MAF 0.05 | gzip > $OUTDIR/tmp1.eigen.gz
+	$TABIX -R $OUTDIR/tmp.bed.gz $VCFDIR/chr$CHR.vcf.gz | $PROGDIR/vcf2eigen CONV -N 1 -dose -MAF -1.0 | gzip > $OUTDIR/tmp2.eigen.gz
 	join -1 1 -2 1 <(zcat $OUTDIR/tmp1.eigen.gz | sort -k1 -b) <(zcat $OUTDIR/tmp2.eigen.gz | sort -k1 -b) | sed 's/_/ /g' | sed 's/ /\t/g' | sort -k2 -n | gzip >> $OUTDIR/eigen.orig.gz
 done
 rm $OUTDIR/tmp1.eigen.gz $OUTDIR/tmp2.eigen.gz
@@ -29,6 +36,6 @@ do
 done > $OUTDIR/ind
 
 # SMARTPCA
-/nfs/team205/nk5/Applications/EIG/bin/smartpca.perl -i $OUTDIR/genotype -a $OUTDIR/snp -b $OUTDIR/ind -e $OUTDIR/eval -l $OUTDIR/log -o $OUTDIR/out -p $OUTDIR/plot -m 0
-bash /nfs/team205/nk5/Applications/EthnicityEst/GMC.sh $OUTDIR
+$SMARTPCA -i $OUTDIR/genotype -a $OUTDIR/snp -b $OUTDIR/ind -e $OUTDIR/eval -l $OUTDIR/log -o $OUTDIR/out -p $OUTDIR/plot -m 0
+bash $PROGDIR/GMC.sh $OUTDIR
 
